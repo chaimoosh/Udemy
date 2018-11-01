649383ec2a56e3e1690c008fd6b7c1587aadc8d1
Chaim Reisman <crreisman@gmail.com>
2018-11-01 11:45:22 -0400

    more

--- src/App/variables.java
+++ src/App/variables.java
@@ -4,7 +4,13 @@ public class variables {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int myInt = 47;
+		
+		short myShort = 4535;
+		
+		
+		
+		System.out.println(myInt + myShort);
 	}
 
 }
