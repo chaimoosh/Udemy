d45d8d955134e0e1070cf114389b17b44c52f3ad
Chaim Reisman <crreisman@gmail.com>
2018-11-01 19:09:35 -0400

    More

--- src/App/stringsAndStuff.java
+++ src/App/stringsAndStuff.java
@@ -3,8 +3,8 @@ package App;
 public class stringsAndStuff {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String text = "Face";
+		System.out.println(text);		
 	}
 
 }
--- src/App/variables.java
+++ src/App/variables.java
@@ -5,11 +5,15 @@ public class variables {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int myInt = 47;
-		
 		short myShort = 4535;
+		long myLong = 344444535;
+		double myDouble = 9.99;
+		float myFloat = 43.4f;
 		
+		char myChar = 'y';
+		boolean myBool = true;
 		
-		
+		byte myByte = 127;
 		System.out.println(myInt + myShort);
 	}
 
