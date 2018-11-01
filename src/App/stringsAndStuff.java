4d509ef514510755183b2536a1aa776e1110c403
Chaim Reisman <crreisman@gmail.com>
2018-11-01 19:12:43 -0400

    More

--- src/App/stringsAndStuff.java
+++ src/App/stringsAndStuff.java
@@ -4,7 +4,11 @@ public class stringsAndStuff {
 
 	public static void main(String[] args) {
 		String text = "Face";
+		String blank = "    ";
+		String more = "thing";
+		String greeting = text + blank + more;
 		System.out.println(text);		
+		System.out.println(greeting);
 	}
 
 }
