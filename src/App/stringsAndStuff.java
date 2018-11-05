ddec5582505df657dd42c54900a81dc52bf262e3
Chaim Reisman <crreisman@gmail.com>
2018-11-05 10:43:53 -0500

    More

--- src/App/stringsAndStuff.java
+++ src/App/stringsAndStuff.java
@@ -7,8 +7,9 @@ public class stringsAndStuff {
 		String blank = "    ";
 		String more = "thing";
 		String greeting = text + blank + more;
+		int myInt = 987;
 		System.out.println(text);		
-		System.out.println(greeting);
+		System.out.println(greeting + myInt);
 	}
 
 }
