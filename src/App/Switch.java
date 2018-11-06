976be15c6fb41a7800d6a0f379feb048d1bb2697
Chaim Reisman <crreisman@gmail.com>
2018-11-06 14:31:43 -0500

    more

---
+++ src/App/Switch.java
@@ -0,0 +1,23 @@
+package App;
+
+import java.util.Scanner;
+
+public class Switch {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Please enter a command");
+		String text = input.nextLine();
+		
+		switch(text) {
+		case "start":
+			System.out.println("Started");
+			break;
+		case "stop":
+			System.out.println("Stopped");
+			break;
+			default:
+				System.out.println("Blah");	
+		}
+	}
+}
