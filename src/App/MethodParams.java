aefd4e8c389bb745c0cd3868deb9e269677b656d
Chaim Reisman <crreisman@gmail.com>
2018-11-12 11:49:04 -0500

    more

--- src/App/MethodParams.java
+++ src/App/MethodParams.java
@@ -4,6 +4,14 @@ class Robot {
 	public void speak(String text) {
 		System.out.println(text);
 	}
+	
+	public void jump(int height) {
+		System.out.println("I can jump " + height + " feet!!!");
+	}
+	
+	public void move(String direction, double distance) {
+		System.out.println("I am moving" + distance + direction);
+	}
 }
 
 public class MethodParams {
@@ -12,6 +20,8 @@ public class MethodParams {
 		// TODO Auto-generated method stub
 		Robot sam = new Robot();
 		sam.speak("Hi I'm Sam the Man");
+		sam.jump(45);
+		sam.move("south", 4.67);
 	}
 
 }
---
+++ src/App/SetterMethods.java
@@ -0,0 +1,36 @@
+package App;
+
+class Frog {
+	String name;
+	int age;
+	
+	public void setName(String newName) {
+		name = newName;
+	}
+	
+	public void setAge(int newAge) {
+		age = newAge;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+}
+
+
+public class SetterMethods {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Frog frog1 = new Frog();
+		frog1.setName("Bertie");
+		frog1.setAge(43);
+		System.out.println(frog1.getName());
+		System.out.println(frog1.getAge());
+	}
+
+}
