b9dcc231872aba2125ecdc0615b6056887ed75d3
Chaim Reisman <crreisman@gmail.com>
2018-11-12 11:19:40 -0500

    more

--- src/App/MethodParams.java
+++ src/App/MethodParams.java
@@ -1,10 +1,17 @@
 package App;
 
+class Robot {
+	public void speak(String text) {
+		System.out.println(text);
+	}
+}
+
 public class MethodParams {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Robot sam = new Robot();
+		sam.speak("Hi I'm Sam the Man");
 	}
 
 }
