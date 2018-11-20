4bf71062aa3e8656ec97e012ebdd9c0d081030ef
Chaim Reisman <crreisman@gmail.com>
2018-11-20 11:17:48 -0500

    more

--- src/Packages/Fishy.java
+++ src/Packages/Fishy.java
@@ -1,10 +1,12 @@
 package Packages;
 
 import ocean.Fish;
+import ocean.plants.Algae;
 import ocean.plants.Shark;
 
 
 public class Fishy {
 	Fish gefilteFish = new Fish();
 	Shark sharky = new Shark();
+	Algae al1 = new Algae();
 }
---
+++ src/interfaces/MacPer.java
@@ -0,0 +1,14 @@
+package interfaces;
+
+public class MacPer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Machine mach1 = new Machine();
+		mach1.start();
+		
+		Person person1 = new Person("Bobby");
+		person1.greet();
+	}
+
+}
