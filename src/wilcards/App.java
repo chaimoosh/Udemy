1759009d16f3c3b7b126aadd1f06a5e94190e448
Chaim Reisman <crreisman@gmail.com>
2018-11-28 11:33:51 -0500

    more

--- src/wilcards/App.java
+++ src/wilcards/App.java
@@ -3,26 +3,47 @@ package wilcards;
 import java.util.ArrayList;
 
 class Machine {
-	
+
+	@Override
+	public String toString() {
+		return "I am a machine";
+	}
+
+	public void start () {
+		System.out.println("Machine started");
+	}
 }
 
 class Camera extends Machine {
+	public String toString() {
+		return "I am a camera";
+	}
 	
+	public void snap () {
+		System.out.println("Snap snap");
+	}
 }
 
 public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		ArrayList<String> list = new ArrayList();
-		list.add("1");
-		
+		ArrayList<Machine> list = new ArrayList();
+		list.add(new Machine());
+		list.add(new Machine());
 		showList(list);
+		
+		ArrayList<Camera> list2 = new ArrayList();
+		list2.add(new Camera());
+		list2.add(new Camera());
+		
+		showList(list2);
 	}
 
-	public static void showList(ArrayList<String> list) {
-		for(String value: list) {
+	public static void showList(ArrayList<? extends Machine> list) {
+		for(Machine value: list) {
 			System.out.println(value);
+			value.start();
 		}
 	}
 }
