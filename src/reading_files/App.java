9e0ffe5ef6f4da3da19f3070330a605af0401332
Chaim Reisman <crreisman@gmail.com>
2018-12-05 11:31:48 -0500

    more

--- src/reading_files/App.java
+++ src/reading_files/App.java
@@ -10,10 +10,15 @@ public class App {
 
 	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
-		String fileName = "‎⁨Macintosh HD⁩ ▸ ⁨Users⁩ ▸ ⁨chaim⁩ ▸ ⁨Downloads ▸ Java⁩";
+		String fileName = "‎⁨Macintosh HD⁩ ▸ ⁨Users⁩ ▸ ⁨chaim⁩ ▸ ⁨Downloads ▸ Java⁩.txt";
 		File textFile = new File(fileName);
 		
 		Scanner in = new Scanner(textFile);
+		
+		while(in.hasNextLine()) {
+			String line = in.nextLine();
+			System.out.println(line);
+		}
 	}
 
 }
